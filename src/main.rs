69108aabe6030a3663eae838698287a3d886f080
Kaze Wong <kazewong.physics@gmail.com>
2024-09-23 07:58:40 -0400

    Update main to use seedable rng

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use rand::Rng;
-use rand::seq::SliceRandom;
+use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
 
 fn insertion_sort(arr: &mut [u32]) {
     // Put your code here...
@@ -7,12 +6,12 @@ fn insertion_sort(arr: &mut [u32]) {
 
 #[test]
 fn test_insertion_sort() {
+    let state = 0xdeadbeef;
     let mut vec: Vec<u32> = (1..100).collect();
-    let mut rng = rand::thread_rng();
+    let mut rng = <StdRng as SeedableRng>::seed_from_u64(state);
     vec.shuffle(&mut rng);
     insertion_sort(&mut vec);
     assert_eq!(vec, (1..100).collect::<Vec<u32>>());
 }
 
-fn main() {
-}
+fn main() {}
