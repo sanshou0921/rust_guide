cf610003829bd3d80c9d4a75276cef7a8b5c23c6
Kaze Wong <kazewong.physics@gmail.com>
2024-09-23 10:06:27 -0400

    Just print for now

--- src/main.rs
+++ src/main.rs
@@ -50,12 +50,11 @@ impl<const N_DIM: usize> State<N_DIM> {
 }
 
 fn main() {
-    const N_STEP: usize = 100;
+    const N_STEP: usize = 1000;
     const N_DIM: usize = 2;
     let mut state = State::<N_DIM>::new(0xdeadbeef);
-    let result: [[f64; N_DIM]; N_STEP] = [[0.0; N_DIM]; N_STEP];
     for _ in 0..N_STEP {
         state.take_step();
-        println!("{:?}", state.arr);
+        println!("{:?}, {:?}", state.arr[0], state.arr[1]);
     }
 }
