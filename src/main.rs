d216946ed78b71c9183857743d09aa272b94cc85
Kaze Wong <kazewong.physics@gmail.com>
2024-09-24 12:23:50 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,5 @@ use sampler::State;
 fn main() {
     const N_STEP: usize = 1000;
     const N_DIM: usize = 2;
-    let mut state = State::<N_DIM>::new(0xdeadbeef);
-    for _ in 0..N_STEP {
-        state.take_step();
-        println!("{:?}, {:?}", state.arr[0], state.arr[1]);
-    }
+    // Fill this
 }
